9eaf653cb1aa5df1e935f2b519f15a9d636ff1c4
AR1D <84451649+AlexManyakalo@users.noreply.github.com>
2025-01-18 20:03:44 +0800

    fix: replace operations

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -11,7 +11,7 @@ const numbers = numbersList.querySelectorAll(".btn");
 // variables for calculations
 let result = 0;
 let currentInput = "";
-const operations = new Set(["*", "/", "%", "+", "-"]);
+const operations = new Set(["×", "÷", "%", "+", "-"]);
 const digits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]);
 
 /*============== FUNCTIONS ===============*/
@@ -52,9 +52,9 @@ function inputKeyboard(event) {
 
 function performOperation(a, b, operation) {
   switch (operation) {
-    case "*":
+    case "×":
       return a * b;
-    case "/":
+    case "÷":
       return a / b;
     case "%":
       return a * (b / 100);
@@ -91,7 +91,7 @@ function calculate() {
 
   const numbersArr = currentInput.split(/[\-+*/%]/).map(Number);
 
-  const highOperations = new Set(["*", "/", "%"]);
+  const highOperations = new Set(["×", "÷", "%"]);
   const lowOperations = new Set(["+", "-"]);
 
   const highResult = sortOperations(highOperations, numbersArr, operationsArr);
